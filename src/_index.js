a7f7c10124801be91d42c1d3a77c2be774f2191c
Promise Gurung <promisegurung@gmail.com>
2022-01-08 18:55:28 +0000

    game tracking

---
+++ src/_index.js
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// function Square() {
+//   const [value, setValue] = useState(null);
+
+//   return (
+//     <button className="square" onClick={() => setValue("X")}>
+//       {value}
+//     </button>
+//   );
+// }
+
+
+// Here we are creating a function component called Board and using the useState hook. Then we added a constant variable and inside it added a 
+// [state and function].
+function Board() {
+  const [clickCount, setClickCount] = useState(0);
+
+  // (Array(9).fill(null)) = The Arrya after useState is a complete different array than the one we created as a state. We know this
+  // because it has a different colour and it is also a keyword. This will create a the 9 boxes that will go into our tik tak to. The fill
+  //(null) will make sure all of the show blank on the screen
+  const [array, _] = useState(Array(9).fill(null));
+
+  // (index) = The value of index is the number that is written in onIncreaseClickHandler(0).
+  const onIncreaseClickHandler = (index) => {
+    // if the clickCount is less than 9 (true) and the array [index] is the position within the array. If there is nothing display null. 
+    //===(confirmed value). LEARN ABOUT ARRAY IN DETAILS!!!
+    if (clickCount < 9 && array[index] === null) {
+      // Here we created a new const var called newCount and assigned it the clickCount +1.
+      const newCount = clickCount + 1;
+
+      setClickCount(newCount);
+
+      // LEARN ABOUT REMAINDER OPERATOR. Here we are using the if funtion to make the computer understand to enter O 
+      //when even and X when odd. For example if the number of click is 3 than the code will divide it by 2 (% 2) which which 
+      //will give us a result of .something which means it is an odd number. if it does not return .anything than it is an even number.
+      if (newCount % 2 == 0) {
+        // alert("The number is even.");
+        array[index] = "O";
+      } else {
+        // alert("the number is ODD");
+        array[index] = "X";
+      }
+    }
+  };
+
+  const a = ["a", "b", "c"];
+  const b = ["e", "f", "g"];
+
+  const c = [a, b];
+
+  const d = [...a, ...b];
+
+  console.log("c =", c);
+  console.log("d", d);
+
+  return (
+    <div>
+      {/*
+      className is the .css file and they are being imported from there.
+       
+       We entered the <div className="board-row"> because we are creating three lines of three squares. if we do not use it than there
+        will be issue in the styling.
+
+       className="square" = squares created in the code.
+      */}
+      <div className="status">STATUS</div>
+      <div className="board-row">
+        <div className="square" onClick={() => onIncreaseClickHandler(0)}>
+          {array[0]}
+        </div>
+        <div className="square" onClick={() => onIncreaseClickHandler(1)}>
+          {array[1]}
+        </div>
+        <div className="square" onClick={() => onIncreaseClickHandler(2)}>
+          {array[2]}
+        </div>
+      </div>
+
+      <div className="board-row">
+        <div className="square" onClick={() => onIncreaseClickHandler(3)}>
+          {array[3]}
+        </div>
+        <div className="square" onClick={() => onIncreaseClickHandler(4)}>
+          {array[4]}
+        </div>
+        <div className="square" onClick={() => onIncreaseClickHandler(5)}>
+          {array[5]}
+        </div>
+      </div>
+
+      <div className="board-row">
+        <div className="square" onClick={() => onIncreaseClickHandler(6)}>
+          {array[6]}
+        </div>
+        <div className="square" onClick={() => onIncreaseClickHandler(7)}>
+          {array[7]}
+        </div>
+        <div className="square" onClick={() => onIncreaseClickHandler(8)}>
+          {array[8]}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// class Board extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       squares: Array(9).fill(null),
+//     };
+//   }
+//   renderSquare(i) {
+//     return <Square value={i} />;
+//   }
+
+//   render() {
+//     const status = "Next player: X";
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// ========================================
+
+ReactDOM.render(<Board />, document.getElementById("root"));
+
+// is the page the main page that is given to by the HTML. So basicallay one page of the website.
--- src/index.js
+++ src/index.js
@@ -1,150 +1,102 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// function Square() {
-//   const [value, setValue] = useState(null);
+const playerOne = "X";
+const playerTwo = "O";
 
-//   return (
-//     <button className="square" onClick={() => setValue("X")}>
-//       {value}
-//     </button>
-//   );
-// }
-
-
-// Here we are creating a function component called Board and using the useState hook. Then we added a constant variable and inside it added a 
-// [state and function].
 function Board() {
-  const [clickCount, setClickCount] = useState(0);
+  const [array, setArray] = useState(Array(9).fill(null));
 
-  // (Array(9).fill(null)) = The Arrya after useState is a complete different array than the one we created as a state. We know this
-  // because it has a different colour and it is also a keyword. This will create a the 9 boxes that will go into our tik tak to. The fill
-  //(null) will make sure all of the show blank on the screen
-  const [array, _] = useState(Array(9).fill(null));
+  useEffect(() => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  // (index) = The value of index is the number that is written in onIncreaseClickHandler(0).
-  const onIncreaseClickHandler = (index) => {
-    // if the clickCount is less than 9 (true) and the array [index] is the position within the array. If there is nothing display null. 
-    //===(confirmed value). LEARN ABOUT ARRAY IN DETAILS!!!
-    if (clickCount < 9 && array[index] === null) {
-      // Here we created a new const var called newCount and assigned it the clickCount +1.
-      const newCount = clickCount + 1;
+    console.log("X" === null);
+    // ++ operator is same as + 1 which we did before.
 
-      setClickCount(newCount);
+    // For keyword is used for LOOP. (LEARN IT!!). We gave the value of i to 0. Made the i smaller than lines.length
+    // length is a keyword meaning run the i in loop with the number of lines you have on the array which is 8.
+    // And on each loop add one in the i (i++). After each loop run the second array until all is done.
+    // const [a, b, c] = lines[i] = If the line is line[2] then it will run   [6, 7, 8].
 
-      // LEARN ABOUT REMAINDER OPERATOR. Here we are using the if funtion to make the computer understand to enter O 
-      //when even and X when odd. For example if the number of click is 3 than the code will divide it by 2 (% 2) which which 
-      //will give us a result of .something which means it is an odd number. if it does not return .anything than it is an even number.
-      if (newCount % 2 == 0) {
-        // alert("The number is even.");
-        array[index] = "O";
-      } else {
-        // alert("the number is ODD");
-        array[index] = "X";
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      console.log(array[a] && array[a]); // x
+
+      console.log(array[a] && array[a] === array[b]); // false
+
+      console.log(array[0] === array[2]); // null
+      // x && null => null
+      console.log(array[a] && array[a] === array[b] && array[a] === array[c]); // false
+      // The code below is used to make sure that when there is a null in array the game does not finish.
+      if (array[a] && array[a] === array[b] && array[a] === array[c]) {
+        // (null && null) === (null && null) === null (promise)
+        // to caluclate this you firs need to find the result of (array[a] && array[a] and compare the result of that to
+        //array[b] && array[a] than compare that result to === array[c]). Why because if you look close you will see
+        //=== operator. && operator result e.g will be x && x = X. x===x result will be true.
+        alert("game");
       }
+    }
+  }, [array]);
+
+  const [currentPlayer, setCurrentPlayer] = useState(playerOne);
+
+  const onSquareClickedHandler = (index) => {
+    if (array[index] !== null) return;
+
+    if (currentPlayer === playerOne) {
+      const newArray = [...array];
+      newArray[index] = playerOne;
+      setArray(newArray);
+
+      setCurrentPlayer(playerTwo);
+    } else {
+      const newArray = [...array];
+      newArray[index] = playerTwo;
+      setArray(newArray);
+
+      setCurrentPlayer(playerOne);
     }
   };
 
-  const a = ["a", "b", "c"];
-  const b = ["e", "f", "g"];
-
-  const c = [a, b];
-
-  const d = [...a, ...b];
-
-  console.log("c =", c);
-  console.log("d", d);
+  const Square = ({ index }) => (
+    <div className="square" onClick={() => onSquareClickedHandler(index)}>
+      {array[index]}
+    </div>
+  );
 
   return (
     <div>
-      {/*
-      className is the .css file and they are being imported from there.
-       
-       We entered the <div className="board-row"> because we are creating three lines of three squares. if we do not use it than there
-        will be issue in the styling.
-
-       className="square" = squares created in the code.
-      */}
-      <div className="status">STATUS</div>
+      <div className="status">{`It is player ${currentPlayer} turn`}</div>
       <div className="board-row">
-        <div className="square" onClick={() => onIncreaseClickHandler(0)}>
-          {array[0]}
-        </div>
-        <div className="square" onClick={() => onIncreaseClickHandler(1)}>
-          {array[1]}
-        </div>
-        <div className="square" onClick={() => onIncreaseClickHandler(2)}>
-          {array[2]}
-        </div>
+        <Square index={0} />
+        <Square index={1} />
+        <Square index={2} />
       </div>
 
       <div className="board-row">
-        <div className="square" onClick={() => onIncreaseClickHandler(3)}>
-          {array[3]}
-        </div>
-        <div className="square" onClick={() => onIncreaseClickHandler(4)}>
-          {array[4]}
-        </div>
-        <div className="square" onClick={() => onIncreaseClickHandler(5)}>
-          {array[5]}
-        </div>
+        <Square index={3} />
+        <Square index={4} />
+        <Square index={5} />
       </div>
 
       <div className="board-row">
-        <div className="square" onClick={() => onIncreaseClickHandler(6)}>
-          {array[6]}
-        </div>
-        <div className="square" onClick={() => onIncreaseClickHandler(7)}>
-          {array[7]}
-        </div>
-        <div className="square" onClick={() => onIncreaseClickHandler(8)}>
-          {array[8]}
-        </div>
+        <Square index={6} />
+        <Square index={7} />
+        <Square index={8} />
       </div>
     </div>
   );
 }
 
-// class Board extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       squares: Array(9).fill(null),
-//     };
-//   }
-//   renderSquare(i) {
-//     return <Square value={i} />;
-//   }
-
-//   render() {
-//     const status = "Next player: X";
-
-//     return (
-//       <div>
-//         <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// ========================================
-
 ReactDOM.render(<Board />, document.getElementById("root"));
-
-// is the page the main page that is given to by the HTML. So basicallay one page of the website.
